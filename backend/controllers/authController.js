e1fccf04c1f66125b1fdb42eb0aad1d61515a386
jamiu <aarejamiu1@gmail.com>
2026-05-03 23:20:51 +0100

    login page

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -32,4 +32,38 @@ const registerUser = async (req, res) => {
     }
 };
 
-module.exports = { registerUser };
+const loginUser = async (req, res) => {
+    const { email, password } = req.body;
+
+    try{
+        const user = await User.findOne({email});
+        if (!user) {
+            return res.status(400).json({message: 'Invalid email'});
+        }
+
+        const isMatch = await bcrypt.compare(password, user.password);
+        if (!isMatch) {
+            return res.status(400).json({message: 'Invalid password'});
+        }
+
+        const token = jwt.sign(
+        { id: user._id },
+        process.env.JWT_SECRET,
+        { expiresIn: "1d" }
+        );
+
+        res.json({ token, user: {
+            id: user._id,
+            username: user.username,
+            email: user.email
+        }});
+    }catch (error) {
+        console.error('Error:', error);
+        res.status(500).json({ message: 'Server error' });
+    }
+}
+
+module.exports = { 
+    registerUser, 
+    loginUser 
+};
--- frontend/js/auth.js
+++ frontend/js/auth.js
@@ -36,3 +36,40 @@ try{
     console.error('Error:', error);
     message.textContent = 'An error occurred during registration.';
 }
+
+// Login form handling
+const loginForm = document.getElementById('loginForm');
+const loginMessage = document.getElementById('message');
+
+if (loginForm) {
+    loginForm.addEventListener('submit', async (e) => {
+        e.preventDefault();
+        let email = loginForm.email.value;
+        let password = loginForm.password.value;
+
+        try {
+            const res = await fetch('/api/auth/login', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify({ email, password })
+            });
+
+            const data = await res.json();
+
+            if (res.ok) {
+                loginMessage.textContent = "Login successful!";
+
+                localStorage.setItem('token', data.token);
+                
+                window.location.href = 'dashboard.html';
+            }else{
+                loginMessage.textContent = data.message || 'Incorrect email or password.';
+            }
+        }catch (error) {console.error('Error:', error);
+            loginMessage.textContent = 'Server error.';
+        }
+    });
+
+}
